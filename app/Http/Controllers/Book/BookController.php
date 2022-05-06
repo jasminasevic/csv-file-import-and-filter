eae5cf9a4878afb7f1550f1569d7c1152b989948
jasmina <jasminasevic@yahoo.com>
2022-05-06 22:43:37 +0200

    change the name of method that gets all books

--- app/Http/Controllers/Book/BookController.php
+++ app/Http/Controllers/Book/BookController.php
@@ -8,7 +8,7 @@ use App\Models\BookModel;
 
 class BookController extends Controller
 {
-    public function book(){
+    public function getAllBooks(){
         return response()->json(BookModel::get(), 200);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('book', 'App\Http\Controllers\Book\BookController@book');
+Route::get('book', 'App\Http\Controllers\Book\BookController@getAllBooks');
