7e0d7e77b4a5dd1205a1c25829ab89d3aabc010e
jasmina <jasminasevic@yahoo.com>
2022-05-10 02:35:04 +0200

    add JWT login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,14 +4,14 @@ namespace App\Http\Controllers;
 
 use http\Env\Response;
 use Illuminate\Http\Request;
-use Auth;
+use Tymon\JWTAuth\Contracts\Providers\Auth;
 use Validator;
 use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function _construct(){
-        $this->middleware('auth:api',['except' => ['login', 'register']]);
+    public function __construct(){
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
     }
 
     public function register(Request $request){
@@ -41,5 +41,41 @@ class AuthController extends Controller
 
     public function login(Request $request){
 
+        $validator = Validator::make($request->all(),[
+            'email' => 'required|email',
+            'password' => 'required|string|min:6'
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(),422);
+        }
+
+        $user = User::where([
+            'email' => $request->email,
+        ])->first();
+
+        if(!$user){
+            return response()->json(['error' => 'Unauthorized user'], 401);
+        }
+
+        if (!password_verify($request->password, $user->password)) {
+            return response()->json(['error' => 'Unauthorized user'], 401);
+        }
+
+        if (! $token = auth()->login($user)) {
+            return response()->json(['error' => 'Unauthorized'], 401);
+        }
+
+        return $this->createNewToken($token);
     }
+
+    public function createNewToken($token){
+        return response()->json([
+           'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth()->factory()->getTTL()*60,
+            'user' => auth()->user()
+        ]);
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -28,9 +28,8 @@ Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
 ], function(){
-    Route::post('/register', [AuthController::class, 'register']);
     Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
 });
-
 
 
